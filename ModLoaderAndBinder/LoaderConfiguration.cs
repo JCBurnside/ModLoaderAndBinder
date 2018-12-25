fbd328725473c6835648dfb26544f986ad6c816f
James Burnside <jcburnside97@gmail.com>
2018-12-25 14:06:36 -0500

    Intial setup of main project and rules

---
+++ ModLoaderAndBinder/LoaderConfiguration.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) James Burnside. All rights reserved.
+// Licensed under the MIT license.
+
+namespace ModLoaderAndBinder.Configuration
+{
+    /// <summary>
+    /// Configures the loader.
+    /// </summary>
+    public class LoaderConfiguration
+    {
+    }
+}
